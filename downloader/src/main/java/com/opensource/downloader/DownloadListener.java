032bf287fdf6ced732bd10e8746d12c0c382e5a7
yinglovezhuzhu <yinglovezhuzhu@139.com>
2014-06-10 16:42:24 +0800

    update copyright

--- downloader/src/main/java/com/opensource/downloader/DownloadListener.java
+++ downloader/src/main/java/com/opensource/downloader/DownloadListener.java
@@ -1,22 +1,19 @@
 /*
- * Copyright (C) 2014-3-19 下午2:13:07 The Android Open Source Project
+ * Copyright (C) 2014 The Android Open Source Project.
+ *
+ *        yinglovezhuzhu@gmail.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * Auther：yinglovezhuzhu@gmail.com
- * FileName:DownloadListener.java
- * Date：2014-3-19
- * Version：v1.0
  */
 
 package com.opensource.downloader;
